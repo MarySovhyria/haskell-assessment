bef987b57d4a8e44af043da2fba2e028b351a549
MarySovhyria <111424962+MarySovhyria@users.noreply.github.com>
2025-12-03 14:53:20 +0000

    Completed Step 1

--- Karma.hs
+++ Karma.hs
@@ -48,19 +48,75 @@ data Extension = ExtReverse8 | ExtThree3s | ExtNineClubs
   deriving (Eq, Show)
 
 --------------------------------------------------------------------------------
+-- Helper
+--------------------------------------------------------------------------------
+isSpecial :: Rank -> Bool
+isSpecial r = r `elem` [R2, R8, R10]
+ 
+--------------------------------------------------------------------------------
 -- Step 1 
 --------------------------------------------------------------------------------
 legalPlay :: Maybe Card -> Card -> Bool
+legalPlay Nothing _ = True
+legalPlay (Just topCard) myCard
+  | isSpecial(rank myCard) = True
+  |rank topCard == R7 = rank myCard <= R7
+  | otherwise = rank myCard >= rank topCard
 
 validPlays :: Maybe Card -> Deck -> Deck
+validPlays Nothing myDeck = myDeck
+validPlays _ [] = []
+validPlays topCard (x:xs)
+  | legalPlay topCard x = x : validPlays topCard xs
+  | otherwise           =     validPlays topCard xs
 
 dealCards :: Int -> State GameState Deck
+dealCards n = do 
+  currentState <- get
+  let oldPile = drawPile currentState
+      dealt   = take n oldPile
+      rest    = drop n oldPile
+  put currentState { drawPile = rest }
+  return dealt
 
 giveWastePileTo :: Player -> State GameState ()
+giveWastePileTo player = do
+    currentState <- get
+    let pileToTake = discardPile currentState
+    let updatePlayer = player {hand = hand player ++ pileToTake}
+    let ix             = currentIx currentState
+        updatedPlayers = take ix (players currentState) ++ [updatedPlayer] ++ drop (ix + 1) (players currentState)
+        
+    put currentState {
+      players = updatedPlayers,
+      discardPile = []
+      }
 
 replenishCards :: Player -> State GameState ()
+replenishCards player = do
+    currentState <- get
+    let dp = drawPile currentState
+        ix = currentIx currentState
+        ph = hand player
+        cardsNeeded = 3 - length ph
+        draw = take cardsNeeded dp
+        rest = drop cardsNeeded dp
+        updatedPlayer = player { hand = ph ++ draw }
+        updatedPlayers = take ix (players currentState) ++ [updatedPlayer] ++ drop (ix + 1) (players currentState)
+
+    when (not (null dp) && length ph < 3) $
+        put currentState { players = updatedPlayers, drawPile = rest }
+
+
+
 
 shuffleDeck :: StdGen -> Deck -> Deck
+shuffleDeck _ [] = []
+shuffleDeck gen deck = 
+    let (idx, newGen) = randomR (0, length deck - 1) gen
+       pickedCard = deck !! idx
+       remainingDeck = take idx deck ++ drop (idx + 1) deck
+    in pickedCard : suffleDeck newGen remainingDeck
 
 --------------------------------------------------------------------------------
 -- Step 2 
